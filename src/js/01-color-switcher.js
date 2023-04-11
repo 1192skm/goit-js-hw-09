2ae269c7174db33e0330b72fc444e4dbe2b53ede
Kostia Shaposhnyk <118116537+1192skm@users.noreply.github.com>
2023-04-11 10:11:34 +0300

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+let timerId = null;
+
+startBtn.disabled = false;
+stopBtn.disabled = true;
+
+function onStartBtnClick() {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+    timerId = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function onStopBtnClick() {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
