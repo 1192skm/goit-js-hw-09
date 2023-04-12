3518e391580088b27f1723cf55d0691d486efb79
Kostia Shaposhnyk <118116537+1192skm@users.noreply.github.com>
2023-04-12 22:20:25 +0300

    Refactoring Task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -15,34 +15,97 @@ function onFormSubmit(evt) {
     const position = i;
     createPromise(position, delay)
       .then(({ position, delay } ) => {
-        setTimeout(() => {
           Notiflix.Notify.success(
             `✅ Fulfilled promise ${position} in ${delay}ms`,
           );
           // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-        }, delay);
       })
       .catch(({ position, delay }) => {
-        setTimeout(() => {
           Notiflix.Notify.failure(
             `❌ Rejected promise ${position} in ${delay}ms`
           );
           // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-        }, delay);
       });
-    delay += step;
   }
 }
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.5;
+  
 
   return new Promise((resolve, reject) => {
-  
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.5;
       if (shouldResolve) {
-        resolve({ position, delay })
+        resolve({ position, delay });
       } else {
-        reject({ position, delay })
+        reject({ position, delay });
       }
+    }, delay);
+  delay += step;
   })
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let i = 1; i <= amount; i += 1) {
+//   const position = i;
+//   createPromise(position, delay)
+//     .then(({ position, delay }) => {
+//       setTimeout(() => {
+//       Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//       // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//       }, delay);
+//     })
+//     .catch(({ position, delay }) => {
+//       setTimeout(() => {
+//       Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+//       // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//       }, delay);
+//     });
+//   delay += step;
+// }
+
+
+
+
+
